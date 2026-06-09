a9bac8b9fa5d962532a6a2b2dceabd5ab699a574
kader198 <maguiskader0@gmail.com>
2026-06-09 11:05:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!");
 });
 
 app.listen(port, () => {
